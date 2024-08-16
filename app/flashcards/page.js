dd5e037c21b7c5083539da39cadfb63c863d54cb
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-08-15 23:39:02 -0600

    Update page.js

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-//import { useUser } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs'
 import { useSearchParams } from 'next/navigation'
 import {
   Container,
